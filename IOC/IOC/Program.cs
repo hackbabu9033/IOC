424d625a819892d7ee24b651ba0536a42ed5c1d9
hank <hank@wishingsoft.com>
2020-01-22 18:01:55 +0800

    IOC 練習

---
+++ IOC/IOC/Program.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Data.SqlClient;
+using System.IO;
+
+namespace IOC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IConfiguration config = new ConfigurationBuilder()
+           .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+           .Build();
+
+            string connectionString = config.GetConnectionString("AdventureWorks2017");
+           
+            bool continueExecution = true;
+            do
+            {
+                Console.Write("Enter First Name:");
+                var firstName = Console.ReadLine();
+
+                Console.Write("Enter Last Name:");
+                var lastName = Console.ReadLine();
+
+                Console.Write("Do you want to save it? Y/N: ");
+
+                var wantToSave = Console.ReadLine();
+
+                if (wantToSave.ToUpper() == "Y")
+                    SaveToDB(firstName, lastName, connectionString);
+
+                Console.Write("Do you want to exit? Y/N: ");
+
+                var wantToExit = Console.ReadLine();
+
+                if (wantToExit.ToUpper() == "Y")
+                    continueExecution = false;
+
+            } while (continueExecution);
+        }
+
+        private static void SaveToDB(string firstName, string lastName,string connstr)
+        {
+            using (SqlConnection conn = new SqlConnection(connstr))
+            {
+                conn.Open();
+
+            }
+        }
+    }
+}
